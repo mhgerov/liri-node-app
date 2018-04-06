f1f06e2fb3cb614749efb0f72026548be8d2f42f
Michael Gerov <mhgerov@gmail.com>
2018-04-06 09:58:17 -0400

    my-tweets and spotify-this-song complete

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-console.log('this is loaded');
-
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -11,3 +9,8 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+module.exports = {
+	spotify: exports.spotify,
+	twitter: exports.twitter
+}
--- liri.js
+++ liri.js
@@ -1,1 +1,46 @@
+//import modules
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+
+var keys = require('./keys.js');
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+//filestream to parse data
+const fs = require('fs');
+
+switch (process.argv[2]) {
+	case 'my-tweets':
+		console.log('These are your last 20 tweets:');
+		client.get('statuses/home_timeline', {count:20}, function (error, tweets, response) {
+			if (error) {console.log(error)}
+			for (i in tweets) {
+				console.log(tweets[i].created_at+'\n'+tweets[i].text);
+			}
+		});
+		break;
+	case 'spotify-this-song':
+		spotify.search({type: 'track',query: process.argv[3],limit:1}).then(function (data) {
+			console.log('Artist: '+data.tracks.items[0].artists[0].name);
+			console.log('Song: '+data.tracks.items[0].name);
+			console.log('Album: '+data.tracks.items[0].album.name);
+			console.log('Spotify Link: '+data.tracks.items[0].preview_url);
+			/*
+			var out = JSON.stringify(data);	
+			//output spotify data for analysis
+			fs.writeFile('no_return.txt', out, 'utf-8');
+			*/
+		}).catch(function(err) {
+			console.log('Artist: Ace of Base');
+			console.log('Song: The Sign');
+			console.log('Album: The Sign');
+		});
+		break;
+	case 'movie-this':
+		break;
+	case 'do-what-it-says':
+		break;
+	default:
+		console.log('Please type in a valid command...');
+}
