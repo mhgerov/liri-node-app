ba6c1e208210b626a0f9cc53eb7fd05ae3f17271
Michael Gerov <mhgerov@gmail.com>
2018-04-06 11:42:44 -0400

    completed movie-this

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var request = require('request');
 
 var keys = require('./keys.js');
 var spotify = new Spotify(keys.spotify);
@@ -12,11 +13,13 @@ const fs = require('fs');
 
 switch (process.argv[2]) {
 	case 'my-tweets':
+		//Grab user screen_name based on .env tokens
 		var user;
 		client.get('account/settings', function (err,response) {
 			user = response.screen_name;
 		});
 		
+		//Output user's last 20 tweets	
 		console.log('These are your last 20 tweets:');
 		client.get('statuses/user_timeline', {screen_name:user,count:20}, function (error, tweets, response) {
 			if (error) {console.log(error)}
@@ -32,9 +35,8 @@ switch (process.argv[2]) {
 			console.log('Song: '+data.tracks.items[0].name);
 			console.log('Album: '+data.tracks.items[0].album.name);
 			console.log('Spotify Link: '+data.tracks.items[0].preview_url);
-			/*
+			/* // Used to dissect JSON 
 			var out = JSON.stringify(data);	
-			//output spotify data for analysis
 			fs.writeFile('no_return.txt', out, 'utf-8');
 			*/
 		}).catch(function(err) {
@@ -44,6 +46,24 @@ switch (process.argv[2]) {
 		});
 		break;
 	case 'movie-this':
+		var url ="http://www.omdbapi.com/?apikey=trilogy"
+		//If user doesn't give valid name, use Mr. Nobody
+		process.argv[3] ? url += '&t='+process.argv[3] : url += '&t=Mr. Nobody';
+		url += '&type=movie';
+		request(url, function (err,resp,body) {
+			//Used for development
+			//fs.writeFile('omdb.txt',body,'utf-8');
+			body = JSON.parse(body);
+			console.log('Title: '+body.Title);
+			console.log('Year: '+body.Year);
+			debugger;
+			console.log('IMDB Rating: '+body.Ratings[0].Value);
+			console.log('Rotten Tomatoes Rating: '+body.Ratings[1].Value);
+			console.log('Countries Produced: '+body.Country);
+			console.log('Languages: '+body.Language);
+			console.log(body.Plot);
+			console.log('Actors: '+body.Actors);
+		});
 		break;
 	case 'do-what-it-says':
 		break;
